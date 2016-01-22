e239622c6b04cf86a4bcfb858b1d8b7b908f9c99
Vladislav Y. Kochedykov <vladislav.kochedykov@gmail.com>
2016-01-22 12:54:40 +0500

    added tests for base read/write functions.

--- tests/com/sbpinvertor/tests/modbus/ModbusMasterTest.java
+++ tests/com/sbpinvertor/tests/modbus/ModbusMasterTest.java
@@ -4,13 +4,13 @@ import com.sbpinvertor.modbus.Modbus;
 import com.sbpinvertor.modbus.ModbusMaster;
 
 /**
- * Copyright (c) 2015 JSC "Zavod "Invertor"
+ * Copyright (c) 2015-2016 JSC "Zavod "Invertor"
  * [http://www.sbp-invertor.ru]
  * <p/>
  * This file is part of JLibModbus.
  * <p/>
  * JLibModbus is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * <p/>
@@ -19,7 +19,7 @@ import com.sbpinvertor.modbus.ModbusMaster;
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * <p/>
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * <p/>
  * Authors: Vladislav Y. Kochedykov, software engineer.
@@ -28,17 +28,27 @@ import com.sbpinvertor.modbus.ModbusMaster;
 public class ModbusMasterTest {
 
     public static void main(String[] argv) {
-        //Modbus.createModbusMasterRTU("COM1", SerialPort.BaudRate.BAUD_RATE_115200, 8, 1, SerialPort.Parity.NONE);
-        ModbusMaster m = Modbus.createModbusMasterTCP("127.0.0.1", 502, true);
-        for (;;) {
+        //ModbusMaster m = Modbus.createModbusMasterRTU("COM1", SerialPort.BaudRate.BAUD_RATE_115200, 8, 1, SerialPort.Parity.NONE);
+        ModbusMaster m = Modbus.createModbusMasterTCP("127.0.0.1", true);
+
+        for (int r = 0; r < 5; r++) {
             try {
-                int[] arr = m.readHoldingRegisters(7, 1162, 1);
-                for (int i : arr)
-                    System.out.print(i + " ");
+                Thread.sleep(1000);
+                printRegisters(m.readHoldingRegisters(0, 0, 10));
+                printRegisters(m.readInputRegisters(0, 0, 10));
+                m.writeSingleRegister(0, 0, 69);
+                m.writeMultipleRegisters(0, 0, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+                m.writeMultipleCoils(0, 0, new boolean[]{true, false, true});
                 System.out.println();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private static void printRegisters(int[] ir) {
+        for (int i : ir)
+            System.out.print(i);
+        System.out.println();
     }
 }
