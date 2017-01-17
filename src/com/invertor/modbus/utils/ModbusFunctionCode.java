ce8381d8b3f7550ec456bcda60c838cbf7fbc49a
Vladislav Y. Kochedykov <vladislav.kochedykov@gmail.com>
2017-01-17 16:56:05 +0500

    improve search of a ModbusFunctionCode instance by value.

--- src/com/invertor/modbus/utils/ModbusFunctionCode.java
+++ src/com/invertor/modbus/utils/ModbusFunctionCode.java
@@ -48,6 +48,34 @@ the list of available modbus functions
 43 / 14 (0x2B / 0x0E) Read Device Identification
 */
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * a enum containing all modbus-function-codes listed in the modbus protocol v1.1b:
+ * <p>
+ * 01 (0x01) Read Coils
+ * 02 (0x02) Read Discrete Inputs
+ * 03 (0x03) Read Holding Registers
+ * 04 (0x04) Read Input Registers
+ * 05 (0x05) Write Single Coil
+ * 06 (0x06) Write Single Register
+ * 07 (0x07) Read Exception Status (Serial Line only)
+ * 08 (0x08) Diagnostics (Serial Line only)
+ * 11 (0x0B) Get Comm Event Counter (Serial Line only)
+ * 12 (0x0C) Get Comm Event Log (Serial Line only)
+ * 15 (0x0F) Write Multiple Coils
+ * 16 (0x10) Write Multiple registers
+ * 17 (0x11) Report Slave ID (Serial Line only)
+ * 20 (0x14) Read File Record
+ * 21 (0x15) Write File Record
+ * 22 (0x16) Mask Write Register
+ * 23 (0x17) Read/Write Multiple registers
+ * 24 (0x18) Read FIFO Queue
+ * 43 ( 0x2B) Encapsulated Interface Transport
+ * 43 / 13 (0x2B / 0x0D) CAN open General Reference Request and Response PDU
+ * 43 / 14 (0x2B / 0x0E) Read Device Identification
+ */
 public enum ModbusFunctionCode {
     READ_COILS(0x1),
     READ_DISCRETE_INPUTS(0x2),
@@ -67,10 +95,26 @@ public enum ModbusFunctionCode {
     MASK_WRITE_REGISTER(0x16),
     READ_WRITE_MULTIPLE_REGISTERS(0x17),
     READ_FIFO_QUEUE(0x18),
-    ENCAPSULATED_INTERFACE_TRANSPORT(0x2B),//implemented Read Device Identification interface, (0x2B / 0x0E)
+    ENCAPSULATED_INTERFACE_TRANSPORT(0x2B),
+    /**
+     * this function code is not presented in the modbus protocol specification.
+     * <code>ModbusFunctionCode.get(int value)</code> returns <code>UNKNOWN</code>
+     * if <code>value</code> is not listed in <code>ModbusFunctionCode</code>.
+     */
     UNKNOWN(0xff);
 
     final static private int MODBUS_EXCEPTION_FLAG = 0x80;
+    /**
+     * create the map containing all values of this enum for the optimization purpose.
+     */
+    final static private Map<Integer, ModbusFunctionCode> values = new HashMap<Integer, ModbusFunctionCode>(ModbusFunctionCode.values().length);
+
+    static {
+        for (ModbusFunctionCode func : ModbusFunctionCode.values()) {
+            values.put(func.value, func);
+        }
+    }
+
     final private int value;
 
     ModbusFunctionCode(int value) {
@@ -81,10 +125,9 @@ public enum ModbusFunctionCode {
         if (isException(value)) {
             value &= ~MODBUS_EXCEPTION_FLAG;
         }
-        for (ModbusFunctionCode func : ModbusFunctionCode.values()) {
-            if (func.value == value) {
-                return func;
-            }
+
+        if (values.containsKey(value)) {
+            return values.get(value);
         }
         return UNKNOWN;
     }
