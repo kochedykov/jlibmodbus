f47c250f7f72b07c471ee71f2cbdfbd8f54bdf2f
Vladislav Y. Kochedykov <vladislav.kochedykov@gmail.com>
2017-01-18 10:20:03 +0500

    update version.

--- src/com/invertor/modbus/Version.java
+++ src/com/invertor/modbus/Version.java
@@ -29,7 +29,7 @@ package com.invertor.modbus;
  */
 class Version {
 	
-	private static final String version = "1.2.3";
+	private static final String version = "1.2.4";
 	/**
 	 * returns the current version of the JLibModbus library
 	 *
