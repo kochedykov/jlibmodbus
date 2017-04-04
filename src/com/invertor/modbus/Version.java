330b84bb109b83bf9d520105fd687e3a98cde1bc
Vladislav Y. Kochedykov <vladislav.kochedykov@gmail.com>
2017-04-04 19:55:04 +0500

    new version 1.2.6 includes some bug fixes and support for purejavacomm.

--- src/com/invertor/modbus/Version.java
+++ src/com/invertor/modbus/Version.java
@@ -29,7 +29,7 @@ package com.invertor.modbus;
  */
 class Version {
 	
-	private static final String version = "1.2.5";
+	private static final String version = "1.2.6";
 	/**
 	 * returns the current version of the JLibModbus library
 	 *
