8d166c3cf7ce4030f883d572d1392970b80552a4
Vladislav Y. Kochedykov <vladislav.kochedykov@gmail.com>
2017-04-24 11:04:37 +0500

    new release 1.2.8. NEWS: * added isConnected, which returns true if a master is connected to a remote slave. * added isListening method, which returns true if a slave is already listening for incoming connections.

--- src/com/invertor/modbus/Version.java
+++ src/com/invertor/modbus/Version.java
@@ -29,7 +29,7 @@ package com.invertor.modbus;
  */
 class Version {
 	
-	private static final String version = "1.2.7";
+	private static final String version = "1.2.8";
 	/**
 	 * returns the current version of the JLibModbus library
 	 *
