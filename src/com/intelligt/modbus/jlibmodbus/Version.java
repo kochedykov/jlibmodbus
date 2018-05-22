2ffdded2aae9eccadac248e70a038b36676b977f
Vladislav Y. Kochedykov <vladislav.kochedykov@gmail.com>
2018-05-22 13:42:07 +0500

    Update version to 1.2.9.5.

--- src/com/intelligt/modbus/jlibmodbus/Version.java
+++ src/com/intelligt/modbus/jlibmodbus/Version.java
@@ -29,7 +29,7 @@ package com.intelligt.modbus.jlibmodbus;
  */
 class Version {
 	
-	private static final String version = "1.2.9.4";
+	private static final String version = "1.2.9.5";
 	/**
 	 * returns the current version of the JLibModbus library
 	 *
