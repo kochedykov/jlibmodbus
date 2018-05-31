d77618946a4ed7a001633bfd1fb11fcbba88f024
Vladislav Y. Kochedykov <vladislav.kochedykov@gmai.com>
2018-05-31 15:40:22 +0500

    Add Android Things support.

---
+++ src/com/intelligt/modbus/jlibmodbus/serial/SerialPortAT.java
@@ -0,0 +1,125 @@
+package com.intelligt.modbus.jlibmodbus.serial;
+
+import com.google.android.things.pio.PeripheralManager;
+import com.google.android.things.pio.UartDevice;
+import com.intelligt.modbus.jlibmodbus.Modbus;
+import com.intelligt.modbus.jlibmodbus.utils.ByteFifo;
+
+import java.io.IOException;
+
+/*
+ * Copyright (C) 2017 Vladislav Y. Kochedykov
+ *
+ * [http://jlibmodbus.sourceforge.net]
+ *
+ * This file is part of JLibModbus.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Authors: Vladislav Y. Kochedykov, software engineer.
+ * email: vladislav.kochedykov@gmail.com
+ */
+
+public class SerialPortAT extends SerialPort {
+
+    private UartDevice port;
+    byte[] buffer = new byte[Modbus.MAX_PDU_LENGTH];
+    volatile long readTime = 0;
+
+    public SerialPortAT(SerialParameters sp) {
+        super(sp);
+        port = null;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        try {
+            port.write(new byte[]{(byte)(b&0xff)}, 1);
+        } catch (Exception e) {
+            throw new IOException(e);
+        }
+    }
+
+    @Override
+    public void write(byte[] bytes) throws IOException {
+        try {
+            port.write(bytes, bytes.length);
+        } catch (Exception e) {
+            throw new IOException(e);
+        }
+    }
+
+    @Override
+    public void open() throws SerialPortException {
+        try {
+            SerialParameters sp = getSerialParameters();
+            port = PeripheralManager.getInstance().openUartDevice(sp.getDevice());
+            port.setBaudrate(sp.getBaudRate());
+            port.setDataSize(sp.getDataBits());
+            port.setParity(sp.getParity().getValue());
+            port.setStopBits(sp.getStopBits());
+            port.setHardwareFlowControl(UartDevice.HW_FLOW_CONTROL_NONE);
+        } catch (Exception ex) {
+            throw new SerialPortException(ex);
+        }
+    }
+
+    @Override
+    public int read() throws IOException {
+        try {
+            readTime = System.currentTimeMillis();
+            while ((System.currentTimeMillis() - readTime) < getReadTimeout() && port.read(buffer, 1) != 1);
+            return buffer[0];
+        } catch (Exception e) {
+            throw new IOException(e);
+        }
+    }
+
+    @Override
+    public int read(byte[] b, int off, final int len) throws IOException {
+        try {
+            readTime = System.currentTimeMillis();
+            int count;
+
+            count = port.read(buffer, buffer.length);
+            while ((System.currentTimeMillis() - readTime) < getReadTimeout() && count > 0) {
+                System.arraycopy(buffer, 0, b, off, count);
+                off += count;
+                //byteFifo.write(buffer, 0, count);
+                count = port.read(buffer, buffer.length);
+            }
+            return off;
+        } catch (Exception e) {
+            throw new IOException(e);
+        }
+    }
+
+    @Override
+    public void close() {
+        if (isOpened()) {
+            try {
+                synchronized (this) {
+                    port.close();
+                    port = null;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    synchronized public boolean isOpened() {
+        return port != null;
+    }
+}
---
+++ src/com/intelligt/modbus/jlibmodbus/serial/SerialPortFactoryAT.java
@@ -0,0 +1,52 @@
+package com.intelligt.modbus.jlibmodbus.serial;
+
+import com.google.android.things.AndroidThings;
+import com.google.android.things.pio.PeripheralManager;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * Copyright (C) 2017 Vladislav Y. Kochedykov
+ *
+ * [http://jlibmodbus.sourceforge.net]
+ *
+ * This file is part of JLibModbus.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Authors: Vladislav Y. Kochedykov, software engineer.
+ * email: vladislav.kochedykov@gmail.com
+ */
+public class SerialPortFactoryAT extends SerialPortAbstractFactory {
+
+    public SerialPortFactoryAT() {
+        super("com.google.android.things.AndroidThings", "androidthings");
+    }
+
+    @Override
+    public SerialPort createSerialImpl(SerialParameters sp) {
+        return new SerialPortAT(sp);
+    }
+
+    @Override
+    public List<String> getPortIdentifiersImpl() {
+        final PeripheralManager pm = PeripheralManager.getInstance();
+        return pm.getUartDeviceList();
+    }
+
+    @Override
+    public String getVersion() {
+        return available() ? AndroidThings.RELEASE : super.getVersion();
+    }
+}
