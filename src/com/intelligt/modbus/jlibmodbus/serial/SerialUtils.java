0ad62fdcd311cc1f5266c1cd64ceab65adaed1b6
Vladislav Y. Kochedykov <vladislav.kochedykov@gmai.com>
2018-05-31 15:40:45 +0500

    Add the automatic selection of a serial port connector.

--- src/com/intelligt/modbus/jlibmodbus/serial/SerialUtils.java
+++ src/com/intelligt/modbus/jlibmodbus/serial/SerialUtils.java
@@ -26,9 +26,69 @@ import java.util.List;
 public class SerialUtils {
 
     static private SerialPortAbstractFactory factory;
+    final static private SerialPortFactoryJSSC FACTORY_JSSC = new SerialPortFactoryJSSC();
+    final static private SerialPortFactoryRXTX FACTORY_RXTX = new SerialPortFactoryRXTX();
+    final static private SerialPortFactoryJSerialComm FACTORY_JSERIAL_COMM = new SerialPortFactoryJSerialComm();
+    final static private SerialPortFactoryPJC FACTORY_PJC = new SerialPortFactoryPJC();
+    final static private SerialPortFactoryAT FACTORY_AT = new SerialPortFactoryAT();
+    @Deprecated
+    final static private SerialPortFactoryJavaComm FACTORY_JAVA_COMM = new SerialPortFactoryJavaComm();
+
+    final static private SerialPortAbstractFactory[] SERIAL_FACTORIES = new SerialPortAbstractFactory[] {
+            FACTORY_JSERIAL_COMM, FACTORY_JSSC, FACTORY_RXTX, FACTORY_PJC, FACTORY_AT, FACTORY_JAVA_COMM};
+
+
+    /*
+    static public boolean isThingsDevice(Context context) {
+        final PackageManager pm = context.getPackageManager();
+        return pm.hasSystemFeature(PackageManager.FEATURE_EMBEDDED);
+    }
+    */
 
     static {
-        factory = new SerialPortFactoryJSSC();
+        for (SerialPortAbstractFactory f : SERIAL_FACTORIES) {
+            if (f.available()) {
+                setSerialPortFactory(f);
+                break;
+            }
+        }
+    }
+
+    static public void setSerialPortFactoryJSSC() {
+        setSerialPortFactory(FACTORY_JSSC);
+    }
+
+    static public void setSerialPortFactoryRXTX() {
+        setSerialPortFactory(FACTORY_RXTX);
+    }
+
+    static public void setSerialPortFactoryJSerialComm() {
+        setSerialPortFactory(FACTORY_JSERIAL_COMM);
+    }
+
+    static public void setSerialPortFactoryPureJavaComm() {
+        setSerialPortFactory(FACTORY_PJC);
+    }
+
+    static public void setSerialPortFactoryAndroidThings() {
+        setSerialPortFactory(FACTORY_AT);
+    }
+
+    @Deprecated
+    static public void setSerialPortFactoryJavaComm() {
+        SerialUtils.factory = FACTORY_JAVA_COMM;
+    }
+
+    static public SerialPort createSerial(SerialParameters sp) throws SerialPortException {
+        return getSerialPortFactory().createSerial(sp);
+    }
+
+    static public List<String> getPortIdentifiers() throws SerialPortException {
+        return getSerialPortFactory().getPortIdentifiers();
+    }
+
+    static public String getConnectorVersion() {
+        return getSerialPortFactory().getVersion();
     }
 
     /**
@@ -39,15 +99,7 @@ public class SerialUtils {
         SerialUtils.factory = factory;
     }
 
-    static public SerialPort createSerial(SerialParameters sp) throws SerialPortException {
-        return factory.createSerial(sp);
-    }
-
-    static public List<String> getPortIdentifiers() {
-        return factory.getPortIdentifiers();
-    }
-
-    static public String getConnectorVersion() {
-        return factory.getVersion();
+    static public SerialPortAbstractFactory getSerialPortFactory() {
+        return SerialUtils.factory;
     }
 }
