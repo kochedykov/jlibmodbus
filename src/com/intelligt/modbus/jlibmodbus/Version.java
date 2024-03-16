8e0fae16313aca393d3a55a3ea6eeb0cdf21abf6
kochedykov <vladislav.kochedykov@gmail.com>
2024-03-16 16:39:12 +0500

    This version contains a valid pom file and fixes the issue #94.

--- src/com/intelligt/modbus/jlibmodbus/Version.java
+++ src/com/intelligt/modbus/jlibmodbus/Version.java
@@ -29,7 +29,7 @@ package com.intelligt.modbus.jlibmodbus;
  */
 class Version {
 	
-	private static final String version = "1.2.9.9";
+	private static final String version = "1.2.9.10";
 	/**
 	 * returns the current version of the JLibModbus library
 	 *
